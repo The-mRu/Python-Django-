9f13c08cf6afbfb352950b8871dfe964b9d45c5c
The_mRu <fsrrayhan@gmail.com>
2024-10-23 23:48:14 +0600

    add...

--- projects/blog/blog/urls.py
+++ projects/blog/blog/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from .views import home_main
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_main),
     path('book/', include('book.urls')),
 ]
